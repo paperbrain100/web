7f459b7ac5794dfef506b0882d07d8566591e0fc
nawed2611 <nawed2611@gmail.com>
2023-04-19 17:27:33 +0530

    Add changes to landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,12 @@ import { RoughNotation } from 'react-rough-notation';
 import Link from 'next/link';
 import Layout from './layout';
 import toast, { Toaster } from 'react-hot-toast';
+import { AiOutlineTwitter, AiOutlineGithub } from 'react-icons/ai';
 import Image from 'next/image';
 import Logo from '../public/logo.png';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+
 
 export default function Home() {
   const { user } = useUser()
@@ -37,11 +39,11 @@ export default function Home() {
             <h1 className='opacity-50 text-3xl text-green-900'>PaperBrain</h1>
 
             <ul className='flex gap-x-4 text-green-900 text-sm'>
-              <li className='cursor-pointer p-2 hover:scale-105 transition-all'>Twitter</li>
-              <li className='cursor-pointer p-2 hover:scale-105 transition-all'>GitHub</li>
+              <li className='cursor-pointer hover:scale-105 transition-all'><AiOutlineTwitter size={22} /></li>
+              <li className='cursor-pointer hover:scale-105 transition-all'><AiOutlineGithub size={22} /></li>
             </ul>
           </div>
-          <div className='flex flex-col h-[90vh] items-center justify-center'>
+          <div className='flex flex-col h-[80vh] items-center justify-center'>
             <div className='m-6 mt-0'>
               <Image
                 src={Logo}
@@ -60,13 +62,22 @@ export default function Home() {
               <h1 className='font-bold text-8xl'>PaperBrain</h1>
             </RoughNotation>
             <p className='mt-8 font-extralight text-xl'>
-              Exploring your study papers has never been easier!
+              Exploring research papers has never been easier!
             </p>
             {!active &&
               <button className='p-2 text-white text-md text-center rounded-lg hover:bg-green-700 cursor-pointer bg-green-600 my-6 px-4 hover:scale-105 transition-all'>
                 <Link href='/api/auth/login'>Lets Get Started</Link>
               </button>
             }
+          </div>
+          <div className='bg-inverse2 p-12 pt-0 flex items-center justify-center'>
+            <video
+              className='w-6/7 rounded-md'
+              autoPlay
+              muted
+              loop
+              src='https://user-images.githubusercontent.com/83456083/233063742-ca57e432-a4db-4d65-b7eb-20bd78e3ed72.mp4'
+            />
           </div>
         </div>
       </Layout>
