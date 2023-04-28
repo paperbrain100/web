db357632c35e1968c34db5f751218b0748d6ef07
Md Anzar Ahmad <anzarhps@gmail.com>
2023-04-28 17:49:52 +0530

    add social links

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,68 +36,51 @@ export default function Home() {
       </Head>
       <Layout className='overflow-hidden'>
         <Toaster />
-        <div className='bg-white h-screen'>
+        <div className='bg-gray-100 h-screen'>
           <div className='bg-gray-50 flex items-center justify-between px-6 shadow'>
-            <div className='inline-flex items-center'>
+            <div className='inline-flex items-center mx-8 gap-x-6 my-3'>
               <Image
                 src={Logo}
                 className='rounded-2xl'
                 alt='PaperBrain'
-                width={60}
-                height={60}
+                width={40}
+                height={40}
               />
-              <h1 className='text-3xl font-extrabold text-black mx-8'>
-                PaperBrain
-              </h1>
+              <h1 className='text-2xl font-extrabold text-black'>PaperBrain</h1>
             </div>
-            <button className='p-2 mx-8 text-white text-md text-center rounded-lg hover:bg-gray-800 cursor-pointer bg-black my-4 px-4 hover:scale-105 transition-all'>
+            <button className='p-2 mx-8 text-white text-md text-center rounded-lg cursor-pointer bg-black px-4 hover:scale-105 transition-all'>
               <Link href={active ? "/search" : "/api/auth/login"}>Sign In</Link>
             </button>
-            {/* <ul className='flex gap-x-4 text-green-900 text-sm'>
-              <li className='cursor-pointer hover:scale-105 transition-all'>
-                <AiOutlineTwitter size={22} />
-              </li>
-              <li className='cursor-pointer hover:scale-105 transition-all'>
-                <AiOutlineGithub size={22} />
-              </li>
-            </ul> */}
           </div>
-          {/* <div className='flex flex-col h-[80vh] items-center justify-center'>
-            <div className='m-6 mt-0'>
-              <Image
-                src={Logo}
-                className='rounded-full'
-                alt='PaperBrain'
-                width={64}
-                height={64}
-              />
-            </div>
-            <RoughNotation
-              animationDelay={1000}
-              animationDuration={2000}
-              type='box'
-              show={true}
-            >
-              <h1 className='font-extrabold text-8xl'>PaperBrain</h1>
-            </RoughNotation>
-            <p className='mt-8 font-extralight text-xl'>
-              Exploring research papers has never been easier!
-            </p>
-            {
-              <button className='p-2 text-white text-md text-center rounded-lg hover:bg-green-700 cursor-pointer bg-green-600 my-6 px-4 hover:scale-105 transition-all'>
-                <Link href={active ? "/search" : "/api/auth/login"}>
-                  Lets Get Started
-                </Link>
-              </button>
-            }
-          </div> */}
           <div className='p-12 flex items-center justify-center gap-x-12'>
             <div className='w-[30%]'>
-              <h1 className='font-extrabold text-3xl'>
-                PaperBrain
-                <br />
-                Exploring research papers has never been easier!
-              </h1>
+              <div className='m-4'>
+                <RoughNotation
+                  animationDelay={1000}
+                  animationDuration={2000}
+                  type='box'
+                  show={true}
+                >
+                  <h1 className='font-extrabold text-2xl p-2'>
+                    Exploring research papers has never been easier!
+                  </h1>
+                </RoughNotation>
+              </div>
+              <div className='flex items-center justify-center m-8'>
+                <ul className='flex gap-x-4 text-gray-900 text-sm'>
+                  <Link href='https://twitter.com/__paperbrain' target='blank'>
+                    <li className='cursor-pointer hover:scale-105 transition-all'>
+                      <AiOutlineTwitter size={22} />
+                    </li>
+                  </Link>
+
+                  <Link href='https://github.com/paperbrain100' target='blank'>
+                    <li className='cursor-pointer hover:scale-105 transition-all'>
+                      <AiOutlineGithub size={22} />
+                    </li>
+                  </Link>
+                </ul>
+              </div>
             </div>
             <video
               className='w-[65%] rounded-xl shadow-2xl'
