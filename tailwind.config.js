7831cadc9976cc76e2c711ffe2915f8665f63182
nawed2611 <nawed2611@gmail.com>
2023-08-24 02:10:07 +0530

    Add changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ['class', '.dark-theme'],
-  darkMode: ['class', '.dark-theme'],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    './pages/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
