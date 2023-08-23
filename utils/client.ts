1e6185538828cdce32f6a4bc943e97c507ec4d7c
nawed2611 <nawed2611@gmail.com>
2023-08-24 02:52:17 +0530

    Add changes

--- utils/client.ts
+++ utils/client.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 export const client = axios.create({
-  baseURL: 'https://web-production-f9ae.up.railway.app/',
+  baseURL: 'http://localhost:5000/',
   // baseURL: 'https://paperbrain.onrender.com/',
   headers: {
     'Content-Type': 'application/json',
