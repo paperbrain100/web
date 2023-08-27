880f474bf12d43de0f19fce3a7a34f34f6c49a70
Mohammad Arshad <mdarshad1000@gmail.com>
2023-08-27 10:26:40 +0530

    Update URL

--- utils/client.ts
+++ utils/client.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 export const client = axios.create({
-  baseURL: 'http:localhost:5000/',
+  // baseURL: 'http:localhost:5000/',
+  baseURL: 'http://54.199.57.42/',
   // baseURL: 'https://paperbrain.onrender.com/',
   headers: {
     'Content-Type': 'application/json',
