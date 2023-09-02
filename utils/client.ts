f6bb7ddac562f041f11cbb7e1b8870d7d6f110c5
Mohammad Arshad <mdarshad1000@gmail.com>
2023-09-02 16:15:55 +0530

    Update URL temporarily

--- utils/client.ts
+++ utils/client.ts
@@ -3,7 +3,8 @@ import Cookies from 'js-cookie';
 
 export const client = axios.create({
   // baseURL: 'http:localhost:5000/',
-  baseURL: 'http://ec2-54-199-57-42.ap-northeast-1.compute.amazonaws.com/',
+  // baseURL: 'http://ec2-54-199-57-42.ap-northeast-1.compute.amazonaws.com/',
+  baseURL: 'https://paperbrain-552b437b4df6.herokuapp.com/',
   // baseURL: 'https://paperbrain.onrender.com/',
   headers: {
     'Content-Type': 'application/json',
