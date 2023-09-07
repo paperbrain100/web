6bae1043a0c12d648b605756e7b7330b3b99a837
Mohammad Arshad <mdarshad1000@gmail.com>
2023-09-08 01:50:56 +0530

    Back on render :)

--- utils/client.ts
+++ utils/client.ts
@@ -4,8 +4,7 @@ import Cookies from 'js-cookie';
 export const client = axios.create({
   // baseURL: 'http:localhost:5000/',
   // baseURL: 'http://ec2-54-199-57-42.ap-northeast-1.compute.amazonaws.com/',
-  baseURL: 'https://paperbrain-552b437b4df6.herokuapp.com/',
-  // baseURL: 'https://paperbrain.onrender.com/',
+  baseURL: 'https://paperbrain.onrender.com/',
   headers: {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${Cookies.get('apiKey')}`,
