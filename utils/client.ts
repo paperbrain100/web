902975305075fa529df75a5964218f9f3a1186bf
Mohammad Arshad <mdarshad1000@gmail.com>
2023-09-19 16:47:24 +0530

    Test on prod

--- utils/client.ts
+++ utils/client.ts
@@ -2,9 +2,9 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 export const client = axios.create({
-  // baseURL: 'http:localhost:5000/',
+  baseURL: 'http://localhost:5000/',
   // baseURL: 'http://ec2-54-199-57-42.ap-northeast-1.compute.amazonaws.com/',
-  baseURL: 'https://paperbrain.onrender.com/',
+  // baseURL: 'https://paperbrain.onrender.com/',
   headers: {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${Cookies.get('apiKey')}`,
